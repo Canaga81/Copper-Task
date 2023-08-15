dbdb43172a5fabf42812ba9d665833e9af7d1653
Canaga81 <Canagamemmedov@gmail.com>
2023-08-15 12:04:11 +0400

    Gonderilenler

--- swiper.js
+++ swiper.js
@@ -9,4 +9,18 @@ var swiperStore = new Swiper(".boySwipper", {
         el: ".swiper-pagination",
         clickable: true,
     },
+    breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        1024: {
+          slidesPerView: 3,
+          spaceBetween: 30,
+        },
+    },
 });
